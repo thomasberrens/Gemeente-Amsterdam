01a1bbd03c4d0bb2e9c70be7557d6e532cf3475e
Thomas Berrens <thomas.berrens@me.com>
2023-04-03 14:11:07 +0200

    implemented the flow for the game, added a prototype (interactable) scenario

--- Assets/Script/CutScene/VideoManager.cs
+++ Assets/Script/CutScene/VideoManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 using UnityEngine.Video;
 
@@ -10,13 +11,12 @@ public class VideoManager : MonoBehaviour
     [System.Serializable]
     public class Cutscene
     { 
-        [field: SerializeField]public VideoClip videoClip { get; set; }                // The video clip to play during the cutscene
-       [field: SerializeField] public GameObject[] objectsToDisable { get; set; } // GameObjects to disable during the cutscene
-       [field: SerializeField] public GameObject[] ObjectsToEnable { get; set; } // GameObjects to enable after the cutscene
+        [field: SerializeField]public VideoClip VideoClip { get; set; }                // The video clip to play during the cutscene
     }
 
-    [field: SerializeField]public RawImage RawImage { get; set; } 
-    [field: SerializeField]public List<Cutscene> cutscenes { get; set; }                // List of cutscenes
+    [field: SerializeField] public RawImage RawImage { get; set; } 
+    [field: SerializeField] public List<Cutscene> Cutscenes { get; set; }                // List of cutscenes
+    [field: SerializeField] public UnityEvent OnVideoEnd { get; private set; }
     private bool _cutscenePlaying = false;           // Flag to check if cutscene is currently playing
 
     public void Start()
@@ -24,24 +24,18 @@ public class VideoManager : MonoBehaviour
         PlayCutscene(0);
     }
 
-    public void PlayCutscene(int cutsceneIndex)
+    private void PlayCutscene(int cutsceneIndex)
     {
         if (_cutscenePlaying)
             return;
 
         _cutscenePlaying = true;
 
-        // Disable objects that need to be disabled during the cutscene
-        foreach (GameObject obj in cutscenes[cutsceneIndex].objectsToDisable)
-        {
-            obj.SetActive(false);
-        }
-
         // Play the video clip for the cutscene
         RenderTexture renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
         VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
         videoPlayer.targetTexture = renderTexture;
-        videoPlayer.clip = cutscenes[cutsceneIndex].videoClip;
+        videoPlayer.clip = Cutscenes[cutsceneIndex].VideoClip;
         RawImage.texture = renderTexture;
 
         // Enable the RawImage before playing the video
@@ -59,25 +53,8 @@ public class VideoManager : MonoBehaviour
 
         // Stop the video playback
         vp.Stop();
-
-        // Enable objects that were disabled during the cutscene
-        foreach (GameObject obj in cutscenes[GetPlayingCutsceneIndex()].ObjectsToEnable)
-        {
-            obj.SetActive(true);
-        }
-    }
-
-    private int GetPlayingCutsceneIndex()
-    {
-        // Loop through all the cutscenes and find the one that is currently playing
-        for (int i = 0; i < cutscenes.Count; i++)
-        {
-            if (GetComponent<VideoPlayer>().clip == cutscenes[i].videoClip)
-                return i;
-        }
-
-        // If no cutscene is playing, return -1
-        return -1;
+        
+        OnVideoEnd?.Invoke();
     }
 
     private void Awake()
