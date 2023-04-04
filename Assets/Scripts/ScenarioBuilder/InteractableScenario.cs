524becf1087624c5268458b558392050c986353e
Thomas Berrens <thomas.berrens@me.com>
2023-04-04 21:25:03 +0200

    made scriptable object for interactable scenarios

---
+++ Assets/Scripts/ScenarioBuilder/InteractableScenario.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class InteractableScenario : ScriptableObject {
+        
+    public string Name { get; private set; }
+    public string TextToDisplay { get; private set; }
+    
+    
+    
+    [MenuItem("Assets/Create/InteractableScenarios")]
+    public static void CreateAsset() => AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<InteractableScenario>(), "Assets/InteractableScenarios/NewInteractableScenario.asset");
+    
+}
