f62f564c5cd3ae37ddbd1dc13ddb5db2981d3848
Jason Dikken <jasondikken1@gmail.com>
2023-04-20 14:21:55 +0200

    changed code review feedback

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using JetBrains.Annotations;
 using UnityEngine;
 
 public class MenuHandler : MonoBehaviour
