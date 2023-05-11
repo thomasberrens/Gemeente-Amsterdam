343829c068c141dea8faad1958a3755422e58d9f
Jason Dikken <jasondikken1@gmail.com>
2023-05-11 11:49:38 +0200

    added all button sounds

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> DontDestroyOnLoadObjects = new List<GameObject>();
     public static GameManager Instance {get; private set; }
 
     private void Awake()
     {
         Instance ??= this;
+
+        for (int i = 0; i < DontDestroyOnLoadObjects.Count; i++)
+        {
+            DontDestroyOnLoad(DontDestroyOnLoadObjects[i]);
+        }
     }
     
     /// <summary>
