5ab85d54b8742b6ec1ea90d0231753d7b6596519
Thomas Berrens <thomas.berrens@me.com>
2023-05-25 12:10:14 +0200

    made login scene feedback

--- Assets/Scripts/CutScene/VideoManager.cs
+++ Assets/Scripts/CutScene/VideoManager.cs
@@ -34,7 +34,7 @@ public class VideoManager : MonoBehaviour
         RenderTexture renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
 
         videoPlayer.targetTexture = renderTexture;
-        videoPlayer.url = GameManager.Instance.FILES_URL + videoClip.name + ".mp4";
+        videoPlayer.url = GameManager.Instance.FilesURL + videoClip.name + ".mp4";
         RawImage.texture = renderTexture;
 
         // Enable the RawImage before playing the video
